456555b841bc3d34102efee7edca060154f8bf5b
xypnox <xypnox@gmail.com>
2019-06-19 19:06:40 +0530

    Add client build folder

---
+++ client/build/precache-manifest.95f4b851f74ba6ac75bf2e25deb04703.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e24f7fab625697932fea73587fd6215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa761d6eefa203066d9",
+    "url": "/static/css/main.80f5d36a.chunk.css"
+  },
+  {
+    "revision": "c2a674c71a66088bf5cd",
+    "url": "/static/js/2.60aaa0ad.chunk.js"
+  },
+  {
+    "revision": "3fa761d6eefa203066d9",
+    "url": "/static/js/main.076f6c4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b142cd3db71db16a1c47ec02e2d6c4b5",
+    "url": "/static/media/406-heart-with-ribbon.b142cd3d.svg"
+  },
+  {
+    "revision": "a4586395314765e1198d02d6738522cb",
+    "url": "/static/media/661-sparkles.a4586395.svg"
+  },
+  {
+    "revision": "de4934a3d0722c1cf7cb8450a611cfe1",
+    "url": "/static/media/713-open-book.de4934a3.svg"
+  },
+  {
+    "revision": "d3d1e0e27e519aaa4d71bfce30f99562",
+    "url": "/static/media/74-nauseated-face-2.d3d1e0e2.svg"
+  },
+  {
+    "revision": "05481c48ef293251bd65344813d081ce",
+    "url": "/static/media/back.05481c48.svg"
+  },
+  {
+    "revision": "e154de501fba2ecb02e0f457c4972dcf",
+    "url": "/static/media/undraw_absorbed.e154de50.svg"
+  }
+]);
